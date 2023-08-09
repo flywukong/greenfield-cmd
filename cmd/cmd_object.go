ac79841de49fb9b3d4308e45f77b4a08bf2a9676
flywukong <2229306838@qq.com>
2023-08-09 20:19:08 +0800

    temp test

--- cmd/cmd_object.go
+++ cmd/cmd_object.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 	"time"
 
 	"cosmossdk.io/math"
@@ -378,6 +379,43 @@ func putObject(ctx *cli.Context) error {
 				fmt.Printf("upload: %s to %s \n", objectName, urlInfo)
 				return nil
 			}
+
+			concurrentNumber := 5
+			downloadCount := 5
+			quota0, err := gnfdClient.GetBucketReadQuota(context.Background(), bucketName)
+			if err != nil {
+				return toCmdErr(err)
+			}
+			fmt.Println("get quota;", quota0)
+
+			var wg sync.WaitGroup
+			wg.Add(concurrentNumber)
+
+			for i := 0; i < concurrentNumber; i++ {
+				go func() {
+					defer wg.Done()
+					for i := 0; i < downloadCount; i++ {
+						objectContent, _, err := gnfdClient.GetObject(c, bucketName, objectName, sdktypes.GetObjectOptions{})
+						if err != nil {
+							fmt.Printf("error: %v", err)
+							quota2, _ := gnfdClient.GetBucketReadQuota(context.Background(), bucketName)
+							//	s.NoError(err, quota2)
+							fmt.Printf("quota: %v", quota2)
+						}
+						_, err = io.ReadAll(objectContent)
+						if err != nil {
+							fmt.Println("get object err;", err.Error())
+						}
+					}
+				}()
+			}
+			wg.Wait()
+
+			expectQuotaUsed := int(objectSize) * concurrentNumber * downloadCount * 1024
+			fmt.Println("expect quota:", expectQuotaUsed)
+			quota1, err := gnfdClient.GetBucketReadQuota(context.Background(), bucketName)
+			consumedQuota := quota1.ReadConsumedSize - quota0.ReadConsumedSize
+			fmt.Println("actual quota:", consumedQuota)
 		}
 	}
 
